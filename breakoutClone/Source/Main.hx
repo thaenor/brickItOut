f856ecb5e361247b45f04cb679f3057f8d276699
Francisco Santos <echo.elyst@gmail.com>
2016-06-03 13:21:27 +0100

    commit made in a hurry

--- breakoutClone/Source/Main.hx
+++ breakoutClone/Source/Main.hx
@@ -108,8 +108,9 @@ class Main extends Sprite
 
 			var i:Int;
 			for (i in 0...map.length) {
-				if ( (ball.x > (map[i].x) && ball.x < (map[i].x+150)) && (ball.y > (map[i].y) && ball.y < (map[i].y+15)) ) {
+				if ( (ball.x > (map[i].x) && ball.x < (map[i].x+20)) && (ball.y > (map[i].y) && ball.y < (map[i].y+20)) ) {
 					this.removeChild(map[i]);
+					ballMovement.y *= -1;
 				}
 			}
 		}
---
+++ breakoutClone/Source/note.md
@@ -0,0 +1,4 @@
+#note#
+
+Probably you should remove your block(map[i]) from map also, because seems that you check collision with map elements, not display elements.
+
